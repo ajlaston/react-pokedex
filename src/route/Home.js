9c0d62325d891c0d9a93294e81c4fc2fc274d61b
Adam Laston <adamlaston@Adams-MBP.attlocal.net>
2022-07-13 22:34:20 -0500

    scrolling fixed

--- src/route/Home.js
+++ src/route/Home.js
@@ -48,7 +48,7 @@ function Home() {
 
     //if at bottom of page, page updates with more pokemon
     const handleScroll = (e) => {
-        if (window.innerHeight + document.documentElement.scrollTop === document.body.scrollHeight ) {
+        if (document.body.offsetHeight + document.documentElement.scrollTop === document.body.scrollHeight ) {
             setPage(page + 20);
             console.log("bottom")
         }
